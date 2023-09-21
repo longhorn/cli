14fa35d1232d61a6e1d4cd684b189033fa8a62f1
Derek Su <derek.su@suse.com>
2023-09-21 23:14:18 +0800

    install: add methods for apt, yum and zypper package managers
    Longhorn 6627
    
    Signed-off-by: Derek Su <derek.su@suse.com>

---
+++ pkg/packagemanager/apt/apt.go
@@ -0,0 +1,53 @@
+package apt
+
+import (
+	"time"
+
+	lhns "github.com/longhorn/go-common-libs/namespace"
+	lhtypes "github.com/longhorn/go-common-libs/types"
+)
+
+type Command struct {
+	executor *lhns.Executor
+}
+
+func NewCommand(executor *lhns.Executor) *Command {
+	return &Command{
+		executor: executor,
+	}
+}
+
+// UpdatePackageList updates list of available packages
+func (c *Command) UpdatePackageList() (string, error) {
+	return c.executor.Execute("apt", []string{"update", "-y"}, lhtypes.ExecuteNoTimeout)
+}
+
+// InstallPackage executes the installation command
+func (c *Command) InstallPackage(name string) (string, error) {
+	return c.executor.Execute("apt", []string{"install", name, "-y"}, lhtypes.ExecuteNoTimeout)
+}
+
+// UninstallPackage executes the uninstallation command
+func (c *Command) UninstallPackage(name string) (string, error) {
+	return c.executor.Execute("apt", []string{"remove", name, "-y"}, lhtypes.ExecuteNoTimeout)
+}
+
+// Execute executes the given command with the specified environment variables, binary, and arguments.
+func (c *Command) Execute(binary string, args []string, timeout time.Duration) (string, error) {
+	return c.executor.Execute(binary, args, timeout)
+}
+
+// Modprobe executes the modprobe command
+func (c *Command) Modprobe(module string) (string, error) {
+	return c.executor.Execute("modprobe", []string{module}, lhtypes.ExecuteNoTimeout)
+}
+
+// StartService executes the service start command
+func (c *Command) StartService(name string) (string, error) {
+	output, err := c.executor.Execute("systemctl", []string{"-q", "enable", name}, lhtypes.ExecuteNoTimeout)
+	if err != nil {
+		return output, err
+	}
+
+	return c.executor.Execute("systemctl", []string{"start", name}, lhtypes.ExecuteNoTimeout)
+}
