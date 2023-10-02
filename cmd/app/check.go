9655b97d972f1feeca1224fd85b0fcd8cb367a72
Derek Su <derek.su@suse.com>
2023-10-02 22:15:14 +0800

    check: add skeleton
    Longhorn 6627
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- cmd/app/check.go
+++ cmd/app/check.go
@@ -1,17 +1,22 @@
 package app
 
 import (
+	"os"
+
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
+
+	"github.com/longhorn/longhorn-preflight/pkg/checker"
+	"github.com/longhorn/longhorn-preflight/pkg/types"
 )
 
-func PreflightCheckCmd() cli.Command {
+func PreflightCheckCmd(packageManager types.PackageManager) cli.Command {
 	return cli.Command{
 		Name:  "check",
 		Flags: []cli.Flag{},
 		Usage: "Check environment",
 		Action: func(c *cli.Context) {
-			if err := check(c); err != nil {
+			if err := check(c, packageManager); err != nil {
 				logrus.WithError(err).Fatalf("Failed to run command")
 			}
 
@@ -19,6 +24,27 @@ func PreflightCheckCmd() cli.Command {
 	}
 }
 
-func check(c *cli.Context) error {
+func check(c *cli.Context, packageManager types.PackageManager) error {
+	checker, err := checker.NewChecker(packageManager)
+	if err != nil {
+		return err
+	}
+
+	checker.CheckIscsidService()
+	checker.CheckMultipathService()
+	checker.CheckNFSv4Support()
+	checker.CheckPackagesInstalled(false)
+
+	if os.Getenv("ENABLE_SPDK") == "true" {
+		instructionSets := map[string][]string{
+			"amd64": {"sse4_2"},
+		}
+		checker.CheckCpuInstructionSet(instructionSets)
+
+		checker.CheckHugePages()
+		checker.CheckPackagesInstalled(true)
+		checker.CheckModulesLoaded(true)
+	}
+
 	return nil
 }
--- pkg/command/command.go
+++ pkg/command/command.go
@@ -7,6 +7,9 @@ type CommandInterface interface {
 	InstallPackage(name string) (string, error)
 	UninstallPackage(name string) (string, error)
 	Modprobe(module string) (string, error)
+	CheckModLoaded(module string) error
 	StartService(name string) (string, error)
-	Execute(cbinary string, args []string, timeout time.Duration) (string, error)
+	GetServiceStatus(name string) (string, error)
+	CheckPackageInstalled(name string) (string, error)
+	Execute(binary string, args []string, timeout time.Duration) (string, error)
 }
