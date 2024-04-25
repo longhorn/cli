c385b62d87b0ca701fce5bb8f3b7de0e90c8faf2
Chin-Ya Huang <chin-ya.huang@suse.com>
2024-04-25 17:05:59 +0800

    feat(install): preflight
    longhorn/longhorn-7927
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- pkg/local/preflight/packagemanager/yum.go
+++ pkg/local/preflight/packagemanager/yum.go
@@ -1,17 +1,17 @@
-package pkgmgr
+package packagemanager
 
 import (
 	"time"
 
-	lhns "github.com/longhorn/go-common-libs/ns"
-	lhtypes "github.com/longhorn/go-common-libs/types"
+	lhgons "github.com/longhorn/go-common-libs/ns"
+	lhgotypes "github.com/longhorn/go-common-libs/types"
 )
 
 type YumPackageManager struct {
-	executor *lhns.Executor
+	executor *lhgons.Executor
 }
 
-func NewYumPackageManager(executor *lhns.Executor) *YumPackageManager {
+func NewYumPackageManager(executor *lhgons.Executor) *YumPackageManager {
 	return &YumPackageManager{
 		executor: executor,
 	}
@@ -19,17 +19,17 @@ func NewYumPackageManager(executor *lhns.Executor) *YumPackageManager {
 
 // UpdatePackageList updates list of available packages
 func (c *YumPackageManager) UpdatePackageList() (string, error) {
-	return c.executor.Execute([]string{}, "yum", []string{"update", "-y"}, lhtypes.ExecuteNoTimeout)
+	return c.executor.Execute([]string{}, "yum", []string{"update", "-y"}, lhgotypes.ExecuteNoTimeout)
 }
 
 // InstallPackage executes the installation command
 func (c *YumPackageManager) InstallPackage(name string) (string, error) {
-	return c.executor.Execute([]string{}, "yum", []string{"install", name, "-y"}, lhtypes.ExecuteNoTimeout)
+	return c.executor.Execute([]string{}, "yum", []string{"install", name, "-y"}, lhgotypes.ExecuteNoTimeout)
 }
 
 // UninstallPackage executes the uninstallation command
 func (c *YumPackageManager) UninstallPackage(name string) (string, error) {
-	return c.executor.Execute([]string{}, "yum", []string{"remove", name, "-y"}, lhtypes.ExecuteNoTimeout)
+	return c.executor.Execute([]string{}, "yum", []string{"remove", name, "-y"}, lhgotypes.ExecuteNoTimeout)
 }
 
 // Execute executes the given command with the specified environment variables, binary, and arguments.
@@ -39,31 +39,31 @@ func (c *YumPackageManager) Execute(envs []string, binary string, args []string,
 
 // Modprobe executes the modprobe command
 func (c *YumPackageManager) Modprobe(module string) (string, error) {
-	return c.executor.Execute([]string{}, "modprobe", []string{module}, lhtypes.ExecuteNoTimeout)
+	return c.executor.Execute([]string{}, "modprobe", []string{module}, lhgotypes.ExecuteNoTimeout)
 }
 
 // CheckModLoaded checks if a module is loaded
 func (c *YumPackageManager) CheckModLoaded(module string) error {
-	_, err := c.executor.Execute([]string{}, "grep", []string{module, "/proc/modules"}, lhtypes.ExecuteNoTimeout)
+	_, err := c.executor.Execute([]string{}, "grep", []string{module, "/proc/modules"}, lhgotypes.ExecuteNoTimeout)
 	return err
 }
 
 // StartService executes the service start command
 func (c *YumPackageManager) StartService(name string) (string, error) {
-	output, err := c.executor.Execute([]string{}, "systemctl", []string{"-q", "enable", name}, lhtypes.ExecuteNoTimeout)
+	output, err := c.executor.Execute([]string{}, "systemctl", []string{"-q", "enable", name}, lhgotypes.ExecuteNoTimeout)
 	if err != nil {
 		return output, err
 	}
 
-	return c.executor.Execute([]string{}, "systemctl", []string{"start", name}, lhtypes.ExecuteNoTimeout)
+	return c.executor.Execute([]string{}, "systemctl", []string{"start", name}, lhgotypes.ExecuteNoTimeout)
 }
 
 // GetServiceStatus executes the service status command
 func (c *YumPackageManager) GetServiceStatus(name string) (string, error) {
-	return c.executor.Execute([]string{}, "systemctl", []string{"status", "--no-pager", name}, lhtypes.ExecuteNoTimeout)
+	return c.executor.Execute([]string{}, "systemctl", []string{"status", "--no-pager", name}, lhgotypes.ExecuteNoTimeout)
 }
 
 // CheckPackageInstalled checks if a package is installed
 func (c *YumPackageManager) CheckPackageInstalled(name string) (string, error) {
-	return c.executor.Execute([]string{}, "rpm", []string{"-q", name}, lhtypes.ExecuteNoTimeout)
+	return c.executor.Execute([]string{}, "rpm", []string{"-q", name}, lhgotypes.ExecuteNoTimeout)
 }
--- pkg/local/preflight/packagemanager/zypper.go
+++ pkg/local/preflight/packagemanager/zypper.go
@@ -1,17 +1,17 @@
-package pkgmgr
+package packagemanager
 
 import (
 	"time"
 
-	lhns "github.com/longhorn/go-common-libs/ns"
-	lhtypes "github.com/longhorn/go-common-libs/types"
+	lhgons "github.com/longhorn/go-common-libs/ns"
+	lhgotypes "github.com/longhorn/go-common-libs/types"
 )
 
 type ZypperPackageManager struct {
-	executor *lhns.Executor
+	executor *lhgons.Executor
 }
 
-func NewZypperPackageManager(executor *lhns.Executor) *ZypperPackageManager {
+func NewZypperPackageManager(executor *lhgons.Executor) *ZypperPackageManager {
 	return &ZypperPackageManager{
 		executor: executor,
 	}
@@ -19,17 +19,17 @@ func NewZypperPackageManager(executor *lhns.Executor) *ZypperPackageManager {
 
 // UpdatePackageList updates list of available packages
 func (c *ZypperPackageManager) UpdatePackageList() (string, error) {
-	return c.executor.Execute([]string{}, "zypper", []string{"update", "-y"}, lhtypes.ExecuteNoTimeout)
+	return c.executor.Execute([]string{}, "zypper", []string{"update", "-y"}, lhgotypes.ExecuteNoTimeout)
 }
 
 // InstallPackage executes the installation command
 func (c *ZypperPackageManager) InstallPackage(name string) (string, error) {
-	return c.executor.Execute([]string{}, "zypper", []string{"--non-interactive", "install", name}, lhtypes.ExecuteNoTimeout)
+	return c.executor.Execute([]string{}, "zypper", []string{"--non-interactive", "install", name}, lhgotypes.ExecuteNoTimeout)
 }
 
 // UninstallPackage executes the uninstallation command
 func (c *ZypperPackageManager) UninstallPackage(name string) (string, error) {
-	return c.executor.Execute([]string{}, "zypper", []string{"--non-interactive", "remove", name}, lhtypes.ExecuteNoTimeout)
+	return c.executor.Execute([]string{}, "zypper", []string{"--non-interactive", "remove", name}, lhgotypes.ExecuteNoTimeout)
 }
 
 // Execute executes the given command with the specified environment variables, binary, and arguments.
@@ -39,31 +39,31 @@ func (c *ZypperPackageManager) Execute(envs []string, binary string, args []stri
 
 // Modprobe executes the modprobe command
 func (c *ZypperPackageManager) Modprobe(module string) (string, error) {
-	return c.executor.Execute([]string{}, "modprobe", []string{module}, lhtypes.ExecuteNoTimeout)
+	return c.executor.Execute([]string{}, "modprobe", []string{module}, lhgotypes.ExecuteNoTimeout)
 }
 
 // CheckModLoaded checks if a module is loaded
 func (c *ZypperPackageManager) CheckModLoaded(module string) error {
-	_, err := c.executor.Execute([]string{}, "grep", []string{module, "/proc/modules"}, lhtypes.ExecuteNoTimeout)
+	_, err := c.executor.Execute([]string{}, "grep", []string{module, "/proc/modules"}, lhgotypes.ExecuteNoTimeout)
 	return err
 }
 
 // StartService executes the service start command
 func (c *ZypperPackageManager) StartService(name string) (string, error) {
-	output, err := c.executor.Execute([]string{}, "systemctl", []string{"-q", "enable", name}, lhtypes.ExecuteNoTimeout)
+	output, err := c.executor.Execute([]string{}, "systemctl", []string{"-q", "enable", name}, lhgotypes.ExecuteNoTimeout)
 	if err != nil {
 		return output, err
 	}
 
-	return c.executor.Execute([]string{}, "systemctl", []string{"start", name}, lhtypes.ExecuteNoTimeout)
+	return c.executor.Execute([]string{}, "systemctl", []string{"start", name}, lhgotypes.ExecuteNoTimeout)
 }
 
 // GetServiceStatus executes the service status command
 func (c *ZypperPackageManager) GetServiceStatus(name string) (string, error) {
-	return c.executor.Execute([]string{}, "systemctl", []string{"status", "--no-pager", name}, lhtypes.ExecuteNoTimeout)
+	return c.executor.Execute([]string{}, "systemctl", []string{"status", "--no-pager", name}, lhgotypes.ExecuteNoTimeout)
 }
 
 // CheckPackageInstalled checks if a package is installed
 func (c *ZypperPackageManager) CheckPackageInstalled(name string) (string, error) {
-	return c.executor.Execute([]string{}, "rpm", []string{"-q", name}, lhtypes.ExecuteNoTimeout)
+	return c.executor.Execute([]string{}, "rpm", []string{"-q", name}, lhgotypes.ExecuteNoTimeout)
 }
