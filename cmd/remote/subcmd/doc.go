45a1ad5a7b5523b1b7d8337ac777900a2eafa3a4
Derek Su <derek.su@suse.com>
2024-07-15 03:57:37 +0000

    feat(docs): add subcommand doc for generating documents
    Longhorn 8992
    
    Signed-off-by: Derek Su <derek.su@suse.com>
    (cherry picked from commit 15af4be24c4941196dff2cd0f941b6cddd4dde91)

---
+++ cmd/remote/subcmd/doc.go
@@ -0,0 +1,32 @@
+package subcmd
+
+import (
+	"os"
+
+	"github.com/longhorn/cli/pkg/utils"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+)
+
+func NewCmdDoc() *cobra.Command {
+	return &cobra.Command{
+		Use:   "doc [output directory]",
+		Short: "Generate markdown documentation for the CLI",
+		Long:  "Generate markdown documentation for the CLI and save it to the specified output directory.",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			outputDir := args[0]
+			if err := generateMarkdownDocs(cmd.Parent(), outputDir); err != nil {
+				utils.CheckErr(errors.Wrapf(err, "Failed to generate markdown documentation"))
+			}
+		},
+	}
+}
+
+func generateMarkdownDocs(cmd *cobra.Command, dir string) error {
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+	return doc.GenMarkdownTree(cmd, dir)
+}
