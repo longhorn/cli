973a4700d044b05adc126cb35103cedcaba5eae3
James Lu <james.lu@suse.com>
2024-08-06 15:11:44 +0800

    fix(os): add `amazon linux` and `sle-micro` supports
    ref: longhorn/longhorn 9139
    
    Signed-off-by: James Lu <james.lu@suse.com>
    (cherry picked from commit df6c8c7444da2ab160bd5a9ad64a2c24472274d5)

--- pkg/local/preflight/packagemanager/packagemanager.go
+++ pkg/local/preflight/packagemanager/packagemanager.go
@@ -10,12 +10,13 @@ import (
 type PackageManagerType string
 
 const (
-	PackageManagerUnknown = PackageManagerType("")
-	PackageManagerApt     = PackageManagerType("apt")
-	PackageManagerYum     = PackageManagerType("yum")
-	PackageManagerZypper  = PackageManagerType("zypper")
-	PackageManagerPacman  = PackageManagerType("pacman")
-	// PackageManagerQlist   = PackageManagerType("qlist")
+	PackageManagerUnknown             = PackageManagerType("")
+	PackageManagerApt                 = PackageManagerType("apt")
+	PackageManagerYum                 = PackageManagerType("yum")
+	PackageManagerZypper              = PackageManagerType("zypper")
+	PackageManagerTransactionalUpdate = PackageManagerType("transactional-update")
+	PackageManagerPacman              = PackageManagerType("pacman")
+	// PackageManagerQlist            = PackageManagerType("qlist")
 )
 
 type PackageManager interface {
@@ -38,6 +39,8 @@ func New(pkgMgrType PackageManagerType, executor *lhgons.Executor) (PackageManag
 		return NewYumPackageManager(executor), nil
 	case PackageManagerZypper:
 		return NewZypperPackageManager(executor), nil
+	case PackageManagerTransactionalUpdate:
+		return NewTransactionalUpdatePackageManager(executor), nil
 	case PackageManagerPacman:
 		return NewPacmanPackageManager(executor), nil
 	default:
