320eab82f062d473e69c9e7a588b2890669e08bf
David Cheng <davidcheng0922@gmail.com>
2025-07-10 17:14:40 +0800

    feat(preflight): operation install support option --namespace
    Signed-off-by: David Cheng <davidcheng0922@gmail.com>

--- cmd/remote/subcmd/install.go
+++ cmd/remote/subcmd/install.go
@@ -7,6 +7,8 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"github.com/longhorn/cli/pkg/consts"
 	"github.com/longhorn/cli/pkg/remote/preflight"
 	"github.com/longhorn/cli/pkg/types"
@@ -113,6 +115,7 @@ If a reboot is required, the following message will be displayed:
 	cmd.Flags().IntVar(&preflightInstaller.HugePageSize, consts.CmdOptHugePageSize, 2048, "Specify the huge page size in MiB for SPDK.")
 	cmd.Flags().StringVar(&preflightInstaller.AllowPci, consts.CmdOptAllowPci, "none", fmt.Sprintf("Specify a comma-separated (%s) list of allowed PCI devices. By default, all PCI devices are blocked by a non-valid address.", consts.CmdOptSeperator))
 	cmd.Flags().StringVar(&preflightInstaller.DriverOverride, consts.CmdOptDriverOverride, "", "Userspace driver for device bindings. Override default driver for PCI devices.")
+	cmd.Flags().StringVar(&preflightInstaller.Namespace, consts.CmdOptNamespace, metav1.NamespaceDefault, "Namespace to deploy preflight resources")
 
 	return cmd
 }
