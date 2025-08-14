89127da3e107a62b12f44d99009710b80c1b5b28
bachmanity1 <bachmanity138@gmail.com>
2025-08-14 14:32:55 +0500

    feat: add image registry global option
    replace image registry with global registry
    
    update docs
    
    Signed-off-by: bachmanity1 <bachmanity138@gmail.com>

--- cmd/remote/longhornctl.go
+++ cmd/remote/longhornctl.go
@@ -43,6 +43,7 @@ func newCmdLonghornctl() *cobra.Command {
 	cmd.PersistentFlags().StringVarP(&globalOpts.LogLevel, consts.CmdOptLogLevel, "l", "info", "log level (trace, debug, info, warn, error, fatal, panic)")
 	cmd.PersistentFlags().StringVar(&globalOpts.KubeConfigPath, consts.CmdOptKubeConfigPath, os.Getenv(consts.EnvKubeConfigPath), "Kubernetes config (kubeconfig) path")
 	cmd.PersistentFlags().StringVar(&globalOpts.Image, consts.CmdOptImage, consts.ImageLonghornCli, "Image containing longhornctl-local")
+	cmd.PersistentFlags().StringVar(&globalOpts.ImageRegistry, consts.CmdOptImageRegistry, "", "Registry to apply to all images (CLI, engine, pause, BCI, etc.), replacing any registry already specified in those images.")
 	cmd.PersistentFlags().StringVar(&globalOpts.NodeSelector, consts.CmdOptNodeSelector, "", "Comma-separated list of key=value pairs to match against node labels, selecting the nodes the DaemonSet will run on (e.g. env=prod,zone=us-west).")
 	cmd.PersistentFlags().StringVar(&globalOpts.Namespace, consts.CmdOptNamespace, metav1.NamespaceDefault, "The namespace to run DaemonSet pods.")
 
